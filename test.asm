757540c2f6518b176bbf5b52896e5c194496aa5f
Vidya Sagar Panjri <vidyasagar.panjri@gmail.com>
2017-03-13 21:57:58 -0700

    Update test.asm

--- test.asm
+++ test.asm
@@ -8,12 +8,19 @@
     includelib \masm32\lib\user32.lib
     includelib \masm32\lib\masm32.lib
     include \masm32\include\masm32.inc
+// to initianlise the constants
 .data
+// variable msg ="vidya sagar panjri" with null at end
     msg db "vidya sagar panjri",0
+// actual code
 .code
 start:
+// entering the starting value of variable
     push offset msg
+// calling the output function available in masm.
     call StdOut
+//then push null
     push 0
+//call the exit funtion to exit the process
     call ExitProcess
 end start 
